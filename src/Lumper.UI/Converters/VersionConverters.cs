1c7b9ac0ff89f24a25874a3a23cb9d2a61200c3c
JPaja <jovan.pavlovic99@gmail.com>
2022-09-22 21:38:39 +0200

    Add: Avalonia versioning support

---
+++ src/Lumper.UI/Converters/VersionConverters.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Avalonia.Data.Converters;
+
+namespace Lumper.UI.Converters;
+
+public static class VersionConverters 
+{
+    /// <summary>
+    /// A value converter that returns version string in format "Major".
+    /// </summary>
+    public static readonly IValueConverter ToStringMajor =
+        new FuncValueConverter<Version?, string>(v => v is null ? string.Empty : v.ToString(1));
+
+    /// <summary>
+    /// A value converter that returns version string in format "Major.Minor".
+    /// </summary>
+    public static readonly IValueConverter ToStringMajorMinor =
+        new FuncValueConverter<Version?, string>(v => v is null ? string.Empty : v.ToString(2));
+
+    /// <summary>
+    /// A value converter that returns version string in format "Major.Minor.Build".
+    /// </summary>
+    public static readonly IValueConverter ToStringMajorMinorBuild =
+        new FuncValueConverter<Version?, string>(v => v is null ? string.Empty : v.ToString(3));
+
+    /// <summary>
+    /// A value converter that returns version string in format "Major.Minor.Build.Revision".
+    /// </summary>
+    public static readonly IValueConverter ToStringMajorMinorBuildRevision =
+        new FuncValueConverter<Version?, string>(v => v is null ? string.Empty : v.ToString(4));
+    
+    /// <summary>
+    /// A value converter that returns version.Major.
+    /// </summary>
+    public static readonly IValueConverter GetMajor =
+        new FuncValueConverter<Version?, int?>(v => v?.Major);
+    
+    /// <summary>
+    /// A value converter that returns version.Minor.
+    /// </summary>
+    public static readonly IValueConverter GetMinor =
+        new FuncValueConverter<Version?, int?>(v => v?.Minor);
+    
+    /// <summary>
+    /// A value converter that returns version.Build.
+    /// </summary>
+    public static readonly IValueConverter GetBuild =
+        new FuncValueConverter<Version?, int?>(v => v?.Build);
+    
+    /// <summary>
+    /// A value converter that returns version.Revision.
+    /// </summary>
+    public static readonly IValueConverter GetRevision =
+        new FuncValueConverter<Version?, int?>(v => v?.Revision);
+
+}
--- src/Lumper.UI/ViewModels/MainWindowViewModel.cs
+++ src/Lumper.UI/ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection;
 using System.Text;
 
 namespace Lumper.UI.ViewModels
@@ -7,5 +8,7 @@ namespace Lumper.UI.ViewModels
     public class MainWindowViewModel : ViewModelBase
     {
         public string Greeting => "Welcome to Avalonia!";
+
+        public Version? Version { get; } = Assembly.GetExecutingAssembly().GetName().Version;
     }
 }
